321694b90f0a38e2b81ef50dab739a08165a902d
strackerfoxx <86536635+strackerfoxx@users.noreply.github.com>
2023-09-29 01:50:56 -0600

    row

--- lib/src/screens/counter/counter_functions_screen.dart
+++ lib/src/screens/counter/counter_functions_screen.dart
@@ -14,18 +14,9 @@ class _CounterFunctionsScreenState extends State<CounterFunctionsScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: const Center(
-            child: Text("Counter Functions"),
-          ),
-          // actions: [
-          //   IconButton(
-          //       onPressed: () {
-          //         counter = 0;
-          //         setState(() {});
-          //       },
-          //       icon: const Icon(Icons.refresh_rounded)),
-          // ],
-        ),
+            title: const Center(
+          child: Text("Counter Functions"),
+        )),
         body: Center(
             child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -41,37 +32,43 @@ class _CounterFunctionsScreenState extends State<CounterFunctionsScreen> {
             ),
           ],
         )),
-        floatingActionButton: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            CustomButton(
-              icon: Icons.plus_one,
-              onPressed: () {
-                setState(() {
-                  counter++;
-                });
-              },
-            ),
-            const SizedBox(height: 10),
-            CustomButton(
-              icon: Icons.refresh_rounded,
-              onPressed: () {
-                setState(() {
-                  counter = 0;
-                });
-              },
-            ),
-            const SizedBox(height: 10),
-            CustomButton(
-              icon: Icons.exposure_minus_1_outlined,
-              onPressed: () {
-                setState(() {
-                  if (counter == 0) return;
-                  counter--;
-                });
-              },
-            ),
-          ],
+        floatingActionButton: Padding(
+          padding: const EdgeInsets.only(bottom: 80),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 35),
+                child: CustomButton(
+                  icon: Icons.plus_one,
+                  onPressed: () {
+                    setState(() {
+                      counter++;
+                    });
+                  },
+                ),
+              ),
+              const SizedBox(width: 50),
+              CustomButton(
+                icon: Icons.refresh_rounded,
+                onPressed: () {
+                  setState(() {
+                    counter = 0;
+                  });
+                },
+              ),
+              const SizedBox(width: 50),
+              CustomButton(
+                icon: Icons.exposure_minus_1_outlined,
+                onPressed: () {
+                  setState(() {
+                    if (counter == 0) return;
+                    counter--;
+                  });
+                },
+              ),
+            ],
+          ),
         ));
   }
 }
@@ -90,6 +87,7 @@ class CustomButton extends StatelessWidget {
   Widget build(BuildContext context) {
     return FloatingActionButton(
       // shape: const StadiumBorder(),
+      backgroundColor: Colors.teal,
       onPressed: onPressed,
       child: Icon(icon),
     );
